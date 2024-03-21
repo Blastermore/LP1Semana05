671e63675388e1111278bd8dd7be59ed44e98881
Afonso Cunha <afonsocunha.dev@gmail.com>
2024-03-21 23:14:36 +0000

    Added two variables to store each crono elapsed time and a print for each chrono elapsed time

--- Chronos/Program.cs
+++ Chronos/Program.cs
@@ -19,6 +19,13 @@ namespace Chronos
             crono1.Stop();
             crono2.Stop();
 
+            float ts1 = crono1.ElapsedMilliseconds/1000f;
+            float ts2 = crono2.ElapsedMilliseconds/1000f;
+
+
+            Console.WriteLine($"Time Elapsed on crono1: {ts1:f3}");
+            Console.WriteLine($"Time Elapsed on crono1: {ts2:f3}");
+
         }
     }
 }
