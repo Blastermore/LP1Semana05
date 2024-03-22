b0cf7320838c974ad9e61a87f6bdbf4a59b52255
Afonso Cunha <afonsocunha.dev@gmail.com>
2024-03-22 00:21:20 +0000

    Corrected the limit for the substring

--- MyGame/Enemy.cs
+++ MyGame/Enemy.cs
@@ -43,7 +43,7 @@ namespace MyGame
 
         public void SetName(string name)
         {
-            int length = 8;
+            int length = 9;
             this.name = name.Substring(0,length);
         }
     }
